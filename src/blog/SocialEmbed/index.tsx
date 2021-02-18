8abb74db42b25c42c29126a6a5e664ae907db458
Giorgio Tognoli <giorgio@f2.net>
2021-02-18 16:28:27 +0100

    SocialEmbed: enabled. FeatureBrick: improved. TeamBrick: better spacing

--- src/blog/SocialEmbed/index.tsx
+++ src/blog/SocialEmbed/index.tsx
@@ -1,35 +1,172 @@
-import React from 'react'
-import { types } from 'react-bricks'
-import { Tweet } from 'react-twitter-widgets'
-
+import * as React from 'react'
+import { useEffect, useRef, useState, useContext } from 'react'
+import { types, ReactBricksContext } from 'react-bricks'
+import { bgColors } from '../../website/colors'
 import blockNames from '../blockNames'
+
 import Container from '../layout/Container'
+import Section from '../layout/Section'
 
 export interface SocialEmbedProps {
   id: string
+  placeholder: string
+  align: string
+  cards: string
+  conversation: string
+  theme: string
+  bg: { color: string; className: string }
 }
 
-const SocialEmbed: types.Brick<SocialEmbedProps> = () => {
-  console.log('twttr', (window as any).twttr.widgets.load())
-  console.log('document', document as any)
+const SocialEmbed: types.Brick<SocialEmbedProps> = ({
+  id,
+  placeholder,
+  align,
+  cards,
+  conversation,
+  theme,
+  bg = bgColors.white.value,
+}) => {
+  const twitterEmbedRef = useRef<HTMLDivElement>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const { isDarkColorMode } = useContext(ReactBricksContext)
+
+  useEffect(() => {
+    const isBlackTheme: boolean =
+      theme === 'dark' || (theme === 'auto' && !!isDarkColorMode)
+    const twTheme: string = isBlackTheme ? 'dark' : ''
+
+    if (twitterEmbedRef?.current) {
+      const currentDocument = twitterEmbedRef?.current.ownerDocument
+      const currentWindow = twitterEmbedRef?.current.ownerDocument.defaultView
+
+      var script = currentDocument.createElement('script')
+      script.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+      script.onload = () => {
+        // @ts-ignore
+        const twttr = currentWindow!['twttr']
+        twttr.ready().then(({ widgets }: any) => {
+          // Clear previously rendered tweet before rendering the updated tweet id
+          if (twitterEmbedRef.current) {
+            twitterEmbedRef.current.innerHTML = ''
+          }
+
+          // const { options, onTweetLoadSuccess, onTweetLoadError } = props
+          console.log(id, widgets, {
+            align,
+            cards,
+            conversation,
+            theme,
+          })
+          widgets
+            .createTweetEmbed(id, twitterEmbedRef.current, {
+              align,
+              cards,
+              conversation,
+              theme: twTheme,
+            })
+            .then(() => {
+              setIsLoading(false)
+            })
+        })
+      }
+      currentDocument.body.appendChild(script)
+    }
+  }, [
+    isLoading,
+    id,
+    placeholder,
+    align,
+    cards,
+    conversation,
+    theme,
+    isDarkColorMode,
+  ])
 
   return (
-    <Container>
-      {/* <iframe
-        width="600"
-        height="350"
-        className="mx-auto items-center"
-        src="https://twitframe.com/show?url=https://twitter.com/matfrana/status/1237840583982329857"
-      ></iframe> */}
-      <Tweet tweetId="1237840583982329857" />
-    </Container>
+    <Section bg={bg}>
+      <Container>
+        <div ref={twitterEmbedRef}>{isLoading && placeholder}</div>
+      </Container>
+    </Section>
   )
 }
 SocialEmbed.schema = {
   name: blockNames.SocialEmbed,
   label: 'Twitter Embed',
   getDefaultProps: () => ({
+    bg: {
+      color: '#fff',
+      className: 'bg-white dark:bg-gray-900',
+    },
     id: '1237840583982329857',
+    placeholder: 'Loading Tweet',
+    position: 'center',
+    cards: 'hidden',
+    conversation: 'none',
+    theme: 'auto',
+    align: 'center',
   }),
+  sideEditProps: [
+    {
+      name: 'id',
+      label: 'Tweet ID',
+      type: types.SideEditPropType.Text,
+    },
+    {
+      name: 'placeholder',
+      label: 'Loading Placeholder',
+      type: types.SideEditPropType.Text,
+    },
+    {
+      name: 'align',
+      label: 'Align',
+      type: types.SideEditPropType.Select,
+      selectOptions: {
+        display: types.OptionsDisplay.Select,
+        options: [
+          { value: 'left', label: 'Left' },
+          { value: 'center', label: 'Center' },
+          { value: 'right', label: 'Right' },
+        ],
+      },
+    },
+    {
+      name: 'cards',
+      label: 'Cards',
+      type: types.SideEditPropType.Select,
+      selectOptions: {
+        display: types.OptionsDisplay.Select,
+        options: [
+          { value: '', label: 'Show' },
+          { value: 'hidden', label: 'Hidden' },
+        ],
+      },
+    },
+    {
+      name: 'conversation',
+      label: 'Conversation',
+      type: types.SideEditPropType.Select,
+      selectOptions: {
+        display: types.OptionsDisplay.Select,
+        options: [
+          { value: '', label: 'Show' },
+          { value: 'none', label: 'None' },
+        ],
+      },
+    },
+    {
+      name: 'theme',
+      label: 'Theme',
+      type: types.SideEditPropType.Select,
+      selectOptions: {
+        display: types.OptionsDisplay.Select,
+        options: [
+          { value: 'auto', label: 'Automatic' },
+          { value: 'light', label: 'Light' },
+          { value: 'dark', label: 'Dark' },
+        ],
+      },
+    },
+  ],
 }
 export default SocialEmbed
