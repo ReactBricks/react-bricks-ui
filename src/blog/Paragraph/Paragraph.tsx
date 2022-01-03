1c7dd79dfefa792b0c02e1c99c42cbfefbdacde8
Matteo Frana <f2@f2.net>
2022-01-03 18:09:02 +0100

    Quote in Section and Container; Fixed Paragraph styles

--- src/blog/Paragraph/Paragraph.tsx
+++ src/blog/Paragraph/Paragraph.tsx
@@ -12,7 +12,7 @@ const Paragraph: types.Brick = () => {
           propName="text"
           placeholder="Paragraph..."
           renderBlock={({ children }) => (
-            <p className="text-xl leading-relaxed mb-2 text-gray-700 dark:text-gray-200">
+            <p className="tet-lg leading-relaxed mb-2 text-gray-700 dark:text-gray-200">
               {children}
             </p>
           )}
@@ -50,12 +50,10 @@ const Paragraph: types.Brick = () => {
             )
           }}
           renderUL={({ children }) => (
-            <ul className="list-disc list-inside mt-4 text-xl">{children}</ul>
+            <ul className="list-disc list-inside mt-4 tet-lg">{children}</ul>
           )}
           renderOL={({ children }) => (
-            <ol className="list-decimal list-inside mt-4 font-base">
-              {children}
-            </ol>
+            <ol className="list-decimal list-inside mt-4 tet-lg">{children}</ol>
           )}
           renderLink={({ children, href }) => (
             <Link href={href} className="text-blue-400">
--- src/blog/Quote/Quote.tsx
+++ src/blog/Quote/Quote.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { RichText, types } from 'react-bricks/frontend'
 import blockNames from '../blockNames'
+import Container from '../layout/Container'
+import Section from '../layout/Section'
 
 const Quote: types.Brick = () => {
   return (
-    <div className="text-2xl my-8 pl-6 py-1 border-l-2 border-pink-500">
-      <RichText
-        propName="text"
-        placeholder="Innovation distinguishes between a leader and a follower."
-        renderBlock={({ children }) => (
-          <p className="text-2xl text-gray-400 dark:text-gray-400">
-            {children}
-          </p>
-        )}
-      />
-    </div>
+    <Section>
+      <Container>
+        <div className="text-2xl my-8 pl-6 py-1 border-l-2 border-pink-500">
+          <RichText
+            propName="text"
+            placeholder="Innovation distinguishes between a leader and a follower."
+            renderBlock={({ children }) => (
+              <p className="text-2xl text-gray-400 dark:text-gray-400">
+                {children}
+              </p>
+            )}
+          />
+        </div>
+      </Container>
+    </Section>
   )
 }
 
